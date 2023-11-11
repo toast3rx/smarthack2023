3c67253719777c99cd66130a376cc9369d9d0b46
Iulia Dumitru <iuliadmtru@gmail.com>
2023-11-11 22:37:53 +0200

    Merge changes very wrong

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,4 +1,39 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import AudioContext from "./../contexts/AudioContext";
+import autoCorrelate from "./../libs/AutoCorrelate";
+import {
+  noteFromPitch,
+  centsOffFromPitch,
+  getDetunePercent,
+} from "./../libs/Helpers";
+
+const audioCtx = AudioContext.getAudioContext();
+const analyserNode = AudioContext.getAnalyser();
+const buflen = 2048;
+var buf = new Float32Array(buflen);
+
+const songDuration = 180; //(s)
+const tempo = 1; // (batai / s)
+let startTime;
+// const v_ref = [1000, 2000, 3000]; // hardcodat vectorul de referinta
+let beats_elapsed = 0; // cate batai au trecut
+const beats_freq = 4; // la cate batai esantionez
+const durata_bataie = 1000; // in milisecunde
+let input_values = [];
+const noteStrings = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
 
 const refFiles = ["/data/you_ref.wav", "/data/tried_ref.wav", "/data/fell_ref.wav", "/data/trying_ref.wav"]
 const inputFiles = ["/data/you1.wav", "/data/tried1.wav", "/data/fell1.wav", "/data/trying1.wav"]
@@ -43,10 +78,59 @@ async function loadInputData() {
 }
 
 export const Api = () => {
+    const [source, setSource] = useState(null);
+    const [started, setStart] = useState(false);
+    const [pitchNote, setPitchNote] = useState("C");
+    const [pitchScale, setPitchScale] = useState("4");
+    const [pitch, setPitch] = useState("0 Hz");
+    const [detune, setDetune] = useState("0");
+    const [notification, setNotification] = useState(false);
+
+    const updatePitch = (time) => {
+        const thisTime = Date.now();
+        analyserNode.getFloatTimeDomainData(buf);
+        var ac = autoCorrelate(buf, audioCtx.sampleRate);
+    
+        if (thisTime - startTime >= durata_bataie) {
+        if (beats_elapsed % beats_freq === 0) {
+            console.log(input_values);
+            input_values = [];
+        }
+        console.log("elapsed", thisTime - startTime);
+        startTime = thisTime;
+        beats_elapsed++;
+        console.log("beats elapsed", beats_elapsed);
+        }
+        // daca nu sunt in perioada care imi trebuie, nu am nevoie sa citesc macar datele de la microfon
+        if (beats_elapsed % beats_freq !== 0)
+        return;
+
+        // ma aflu in perioada corecta
+        if (ac > -1) {
+        let note = noteFromPitch(ac);
+        let sym = noteStrings[note % 12];
+        let scl = Math.floor(note / 12) - 1;
+        let dtune = centsOffFromPitch(ac, note);
+        setPitch(parseFloat(ac).toFixed(2) + " Hz");
+        setPitchNote(sym);
+        setPitchScale(scl);
+        setDetune(dtune);
+        setNotification(false);
+        // pun frecventele in vectorul care e dat ca parametru in functia de comparare cu ref-ul
+        input_values.push(ac);
+        }
+    };
+
     useEffect(() => {
         loadRefData();
         loadInputData();
-    }, []);
+
+        if (source != null) {
+        source.connect(analyserNode);
+        // seteaza timpul epoch cand butonul e apasat
+        startTime = Date.now();
+        }
+    }, [source]);
 
     const songDuration = 242;  // 4:02
     const songBPM = 151;  // beats per minute
@@ -120,15 +204,67 @@ export const Api = () => {
         // console.log("trying: " + compareArrays(dataRefa[3], inputData4));
     }
 
+    setInterval(updatePitch, 1);
+
+    const start = async () => {
+        const input = await getMicInput();
+
+        if (audioCtx.state === "suspended") {
+        await audioCtx.resume();
+        }
+        setStart(true);
+        setNotification(true);
+        setTimeout(() => setNotification(false), 5000);
+        setSource(audioCtx.createMediaStreamSource(input));
+    };
+
+    const stop = () => {
+        source.disconnect(analyserNode);
+        console.log("stopped");
+        setStart(false);
+    };
+
+    const getMicInput = () => {
+        return navigator.mediaDevices.getUserMedia({
+        audio: {
+            echoCancellation: true,
+            autoGainControl: false,
+            noiseSuppression: false,
+            latency: 0,
+        },
+        });
+    };
+
     const buttonClick = () => {
         console.log("Button clicked");
     }
 
-    return (
-        <div>
-            <button onClick={buttonClick}> Click me </button>
-            <button onClick={() => {_wavCompare()}}> Test </button>
-        </div>
+    // return (
+    //     <div>
+    //         <button onClick={buttonClick}> Click me </button>
+    //         <button onClick={() => {_wavCompare()}}> Test </button>
+    //     </div>
 
+    // );
+    return (
+        <div className="flex flex-col items-center">
+            {/* <button onClick={buttonClick}> Click me </button>
+            <button onClick={() => {_wavCompare()}}> Test </button> */}
+            {!started ? (
+            <button
+                className="start"
+                onClick={start}
+            >
+                Start
+            </button>
+            ) : (
+            <button
+                className="stop"
+                onClick={stop}
+            >
+                Stop
+            </button>
+            )}
+        </div>
     );
 }
