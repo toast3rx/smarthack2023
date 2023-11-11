eca1629b805b4358088ca278750c28fd665959c1
toaster <latea.alexandru@yahoo.com>
2023-11-11 18:13:22 +0200

    Blank space

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,10 +2,14 @@ import { Grid } from "@mui/material";
 import "./styles/Song.css";
 
 export const Song = () => {
+
+	const blankSpace = " ";
+
   return (
     <Grid container>
       <Grid item xs={12} textAlign={"center"} className = "tab">
-        <h1> Song{' '}  </h1>
+        <p> {blankSpace.repeat(0)} Song </p>
+		<p>1234567890</p>
       </Grid>
     </Grid>
   );
